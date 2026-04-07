946d4ed5817e3f01ae7737da5c32a7b2ef218f65
khushi-2902 <kg700223@gmail.com>
2026-04-07 16:38:05 +0530

    Time: 83 ms (36.25%), Space: 62.2 MB (31.12%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.java
+++ 0879-profitable-schemes/0879-profitable-schemes.java
@@ -1,32 +1,34 @@
 class Solution {
-    int MOD = 1000000007;
-
+   
     public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
-        int dp[][][] = new int[group.length][n + 1][minProfit + 1];
-
-        for (int i = 0; i < group.length; i++) {
-            for (int j = 0; j <= n; j++) {
-                Arrays.fill(dp[i][j], -1);
-            }
-        } return solve(group.length - 1, n, minProfit, group, profit, 0, 0, dp);
+        int dp[][][]=new int[group.length][n+1][minProfit+1];
+         for (int i = 0; i < group.length; i++) {
+              for (int j = 0; j <=n; j++) {
+                 Arrays.fill(dp[i][j], -1);
+              }
+         }
+        int  ans=solve(group.length-1,n,minProfit,group,profit,0,0,dp);
+        return ans;
     }
-
-    public int solve(int idx, int n, int minProfit, int[] group, int[] profit, int members, int currProfit, int[][][] dp) {
-        if (idx < 0) {
-            return currProfit >= minProfit ? 1 : 0;
+    public int  solve(int n,int m,int minProfit,int group[],int profit[],int members,int p,int dp[][][])
+    {
+        //BaseCase
+          if(n < 0)
+        {
+          return (p >= minProfit) ? 1 : 0;
+        }
+        if(dp[n][members][p]!=-1)
+        {
+            return dp[n][members][p];
         }
 
-        if (dp[idx][members][currProfit] != -1) {
-            return dp[idx][members][currProfit];
+        int include=0;
+        if(members+group[n]<=m)
+        {
+            int newProfit=Math.min(p+profit[n],minProfit);
+            include=solve(n-1,m,minProfit,group,profit,members+group[n],newProfit,dp);
         }
-
-        int include = 0;
-        if (members + group[idx] <= n) {
-            int newProfit = Math.min(currProfit + profit[idx], minProfit);
-            include = solve(idx - 1, n, minProfit, group, profit, members + group[idx], newProfit, dp);
-        }
-    int exclude = solve(idx - 1, n, minProfit, group, profit, members, currProfit, dp);
-
-        return dp[idx][members][currProfit] = (include + exclude) % MOD;
+        int exclude=solve(n-1,m,minProfit,group,profit,members,p,dp);
+        return dp[n][members][p]=(include+exclude)%1000000007;
     }
 }
