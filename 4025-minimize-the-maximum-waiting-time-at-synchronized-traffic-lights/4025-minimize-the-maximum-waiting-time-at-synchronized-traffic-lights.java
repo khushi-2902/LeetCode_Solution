e29c73e2620d17f5ff41a7d369d1336e38230597
khushi-2902 <kg700223@gmail.com>
2026-08-20 07:28:57 +0530

    Time: 3 ms (99.83%), Space: 122 MB (6.71%) - LeetHub

--- 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.java
+++ 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.java
@@ -1,54 +1,26 @@
+// 
+
 class Solution {
     public int minPenalty(int period, int[] lights, int[] arrivalTime) {
-       
-       PriorityQueue<int[]> pq = new PriorityQueue<>(
-    (a, b) -> Integer.compare(a[0], b[0])
-);
-        int maxDiff=0;
-        for(int i=0;i<arrivalTime.length;i++)
-        {
-        int r=arrivalTime[i]%period;
 
-        pq.add(new int[]{r,period-r});
-       
-       
+        // Find the light with the maximum green duration
+        int maxLight = 0;
+
+        for (int light : lights) {
+            maxLight = Math.max(maxLight, light);
         }
 
-        //i justhave to assign these differneces to the lightss and the waiting time will only be calculated 
-        //when r[i]>car[i] that is assigned to it;
-       
-     
-     int k=0;
-    while(k<lights.length && !pq.isEmpty())
-    {
-        int poll[]=pq.poll();
-        int r=poll[0];
-        int diff=poll[1];
-        while(k<lights.length && r>=lights[k])
-        {
-            k++;
+        int maxPenalty = 0;
+
+        // Calculate the waiting time for each car
+        for (int time : arrivalTime) {
+            int r = time % period;
+
+            if (r >= maxLight) {
+                maxPenalty = Math.max(maxPenalty, period - r);
+            }
         }
-        if(k>=lights.length)
-        {
-             maxDiff=Math.max(maxDiff,diff);
-        }
-        
-        
-    }
-    while(!pq.isEmpty())
-    {
-        int poll[]=pq.poll();
-        int diff=poll[1];
-        maxDiff=Math.max(maxDiff,diff);
-    }
 
-
-return maxDiff;
-
-
-
-
-
-
+        return maxPenalty;
     }
 }
