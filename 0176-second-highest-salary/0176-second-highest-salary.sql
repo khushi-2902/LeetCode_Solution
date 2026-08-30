323929a97ff817aae94d7c1e99c1617693bc4e23
khushi-2902 <kg700223@gmail.com>
2026-08-30 16:52:16 +0530

    Time: 315 ms (39.24%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -3,7 +3,17 @@
 --  (SELECT salary,DENSE_RANK() 
 -- OVER(ORDER BY salary DESC) as r
 --  FROM Employee) t
--- -- WHERE r=2;
+-- WHERE r=2;)
+
+
+select max(salary) as SecondHighestSalary from 
+(select salary,DENSE_RANK() over(order by salary desc) as r
+from Employee) t
+where r=2;
+
+
+
+
 
 -- SELECT MAX(salary) AS SecondHighestSalary 
 -- FROM Employee 
@@ -16,9 +26,27 @@
 -- WHERE r=2;
 
 
-with old_cte as(select salary from employee),
- new_cte as (select max(salary) as highest_Salary  from Employee),
-second_cte as (select max(salary) as SecondHighestSalary from old_cte where salary< (select highest_Salary from new_cte))
+-- with old_cte as(select salary from employee),
+--  new_cte as (select max(salary) as highest_Salary  from Employee),
+-- second_cte as (select max(salary) as SecondHighestSalary from old_cte where salary< (select highest_Salary from new_cte))
 
- SELECT SecondHighestSalary
-FROM second_cte;
+--  SELECT SecondHighestSalary
+-- FROM second_cte;
+
+
+-- WITH Salary AS (
+--     SELECT salary,
+-- 	DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+--     FROM Employee
+-- )
+-- SELECT salary FROM Salary
+-- WHERE rnk = 3;
+
+
+
+
+
+-- select max(salary) as secondHighestSalary from employee where  salary<(select max(salary) from employee)
+
+-- select MAX(salary) as secondHighestSalary from(
+-- select salary, DENSE_RANK() OVER(ORDER BY salary DESC) as r from employee) T where r=2;
